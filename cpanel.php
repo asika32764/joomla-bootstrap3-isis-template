c6297ae08dbe8ef50313b8f74aa0594e47ba65bd
Simon Asika <asika32764@gmail.com>
2014-03-31 11:30:35 +0800

    Prepare for release

--- cpanel.php
+++ cpanel.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package     Joomla.Administrator
- * @subpackage  Templates.isis
+ * @subpackage  Templates.isis3
  *
  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
--- js/template.js
+++ js/template.js
@@ -1,6 +1,6 @@
 /**
  * @package     Joomla.Administrator
- * @subpackage  Templates.isis
+ * @subpackage  Templates.isis3
  * @copyright   Copyright (C) 2005 - 2014 Open Source Matters, Inc. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  * @since       3.0
